815ecfe99b8de24e225891317a29e1a825a58379
Yewon-Song <supernova2000@naver.com>
2026-01-02 16:26:55 +0900

    hgl에 따른 7개로 라벨 맵핑

--- mmdet3d/configs/_base_/datasets/semantickitti_base.py
+++ mmdet3d/configs/_base_/datasets/semantickitti_base.py
@@ -25,44 +25,42 @@ class_names = [
     'motorcyclist', 'road', 'parking', 'sidewalk', 'other-ground', 'building',
     'fence', 'vegetation', 'trunck', 'terrian', 'pole', 'traffic-sign'
 ]
-
 labels_map = {
-    0:  -1,  # "unlabeled"
-    1:  -1,  # "outlier" -> unlabeled
-    10:  0,  # "car"
-    11: -1,  # "bicycle"
-    13: -1,  # "bus" -> other-vehicle
-    15: -1,  # "motorcycle"
-    16: -1,  # "on-rails" -> other-vehicle
-    18: -1,  # "truck"
-    20: -1,  # "other-vehicle"
-    30:  1,  # "person"
-    31: -1,  # "bicyclist"
-    32: -1,  # "motorcyclist"
-    40:  2,  # "road"
-    44:  2,  # "parking"
-    48:  3,  # "sidewalk"
-    49: -1,  # "other-ground"
-    50:  5,  # "building"
-    51:  5,  # "fence"
-    52: -1,  # "other-structure" -> unlabeled
-    60:  2,  # "lane-marking" -> road
-    70:  6,  # "vegetation"
-    71:  6,  # "trunk"
-    72:  4,  # "terrain"
-    80:  5,  # "pole"
-    81:  5,  # "traffic-sign"
-    99: -1,  # "other-object" -> unlabeled
-    252: 0,  # "moving-car" -> car
-    253: -1, # "moving-bicyclist"
-    254: 1,  # "moving-person" -> person
-    255: -1, # "moving-motorcyclist"
-    256: -1, # "moving-on-rails" -> other-vehicle
-    257: -1, # "moving-bus" -> other-vehicle
-    258: -1, # "moving-truck"
-    259: -1  # "moving-other-vehicle"
+    0: 19,  # "unlabeled"
+    1: 19,  # "outlier" mapped to "unlabeled" --------------mapped
+    10: 0,  # "car"
+    11: 1,  # "bicycle"
+    13: 4,  # "bus" mapped to "other-vehicle" --------------mapped
+    15: 2,  # "motorcycle"
+    16: 4,  # "on-rails" mapped to "other-vehicle" ---------mapped
+    18: 3,  # "truck"
+    20: 4,  # "other-vehicle"
+    30: 5,  # "person"
+    31: 6,  # "bicyclist"
+    32: 7,  # "motorcyclist"
+    40: 8,  # "road"
+    44: 9,  # "parking"
+    48: 10,  # "sidewalk"
+    49: 11,  # "other-ground"
+    50: 12,  # "building"
+    51: 13,  # "fence"
+    52: 19,  # "other-structure" mapped to "unlabeled" ------mapped
+    60: 8,  # "lane-marking" to "road" ---------------------mapped
+    70: 14,  # "vegetation"
+    71: 15,  # "trunk"
+    72: 16,  # "terrain"
+    80: 17,  # "pole"
+    81: 18,  # "traffic-sign"
+    99: 19,  # "other-object" to "unlabeled" ----------------mapped
+    252: 0,  # "moving-car" to "car" ------------------------mapped
+    253: 6,  # "moving-bicyclist" to "bicyclist" ------------mapped
+    254: 5,  # "moving-person" to "person" ------------------mapped
+    255: 7,  # "moving-motorcyclist" to "motorcyclist" ------mapped
+    256: 4,  # "moving-on-rails" mapped to "other-vehic------mapped
+    257: 4,  # "moving-bus" mapped to "other-vehicle" -------mapped
+    258: 3,  # "moving-truck" to "truck" --------------------mapped
+    259: 4  # "moving-other"-vehicle to "other-vehicle"-----mapped
 }
-
 
 metainfo = dict(
     classes=class_names, seg_label_mapping=labels_map, max_label=259)
